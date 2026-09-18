f692c3b9730c427c05736e63e991710b31c80abd
Kamogelo-Kedige <kedigekamogelo32@gmail.com>
2026-09-18 20:46:57 +0200

    Load ATM data in Main startup
    Main now initializes an ATM instance using `ATMDataHandler.loadDataFromFile(...)` with a configured cash capacity, then prints key ATM details (ID, max cash capacity, and location). This wires startup to real ATM data instead of only printing the greeting.

--- src/main/java/com/CashGuard/Main.java
+++ src/main/java/com/CashGuard/Main.java
@@ -1,10 +1,23 @@
 package com.CashGuard;
 
 
+import com.CashGuard.ATM.ATM;
+import com.CashGuard.ATM.ATMDataHandler;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello and welcome! My name is Kamogelo");
+
+        ATM taxiRankATM = ATMDataHandler.loadDataFromFile(
+                "C:\\Users\\kedig\\Documents\\GitHub\\CashGuard\\src\\main\\resources\\atm_mall.csv",
+                500000
+        );
+
+
+        System.out.println("Taxi Rank ATM ID: " + taxiRankATM.getId());
+        System.out.println("Capa:" + taxiRankATM.getMaxCashCapacity());
+        System.out.println("Loca:" + taxiRankATM.getLocation());
 
     }
 }
