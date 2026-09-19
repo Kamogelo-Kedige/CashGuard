9ccbd9c5219133278e8137e1fa3a8e0bfe9134b1
Kamogelo-Kedige <kedigekamogelo32@gmail.com>
2026-09-19 12:37:45 +0200

    Convert Main to JavaFX application entry point
    Replaced the console-based `Main` class with a JavaFX `Application` subclass. The app now launches via `launch(args)` and implements `start(Stage primaryStage)` to initialize and show the primary window titled "CashRunway", establishing the GUI startup flow.

--- src/main/java/com/CashGuard/Main.java
+++ src/main/java/com/CashGuard/Main.java
@@ -1,13 +1,18 @@
 package com.CashGuard;
 
-
+import javafx.application.Application;
+import javafx.stage.Stage;
 import com.CashGuard.ATM.ATM;
 import com.CashGuard.ATM.ATMDataHandler;
 import com.CashGuard.ATM.CashShortagePredictor;
 
-public class Main {
+
+
+public class Main extends Application {
     public static void main(String[] args) {
 
+       launch(args);
+       
         ATM mallATM = ATMDataHandler.loadDataFromFile(
                 "C:\\Users\\kedig\\Documents\\GitHub\\CashGuard\\src\\main\\resources\\atm_taxirank.csv",
                 500000
@@ -22,5 +27,27 @@ public class Main {
         System.out.println("Hours to empty: " + predictor.predictHoursToEmpty(mallATM));
         System.out.println("Risk level: " + predictor.determineRiskLevel(mallATM));
 
+    }
+
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages.
+     * @throws Exception if something goes wrong
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("CashRunway");
+        primaryStage.show();
     }
 }
